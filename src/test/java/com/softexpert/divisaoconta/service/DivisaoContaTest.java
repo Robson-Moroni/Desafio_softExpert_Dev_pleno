b89cfd6741a4ee009295eedf34c73ecf9e7a3288
Morôni Pupo <robson.moroni@hotmail.com>
2024-03-12 09:14:00 -0300

    feature: adicionado test do gerador de link de pagamento

--- src/test/java/com/softexpert/divisaoconta/service/DivisaoContaTest.java
+++ src/test/java/com/softexpert/divisaoconta/service/DivisaoContaTest.java
@@ -1,10 +1,12 @@
 package com.softexpert.divisaoconta.service;
 
+import com.softexpert.divisaoconta.api.dto.DivisaoContaDTO;
 import com.softexpert.divisaoconta.api.model.Conta;
 import com.softexpert.divisaoconta.api.model.ItemPedido;
 import com.softexpert.divisaoconta.api.model.OutroValor;
 import com.softexpert.divisaoconta.api.model.Pedido;
 import com.softexpert.divisaoconta.api.service.CalculadoraPedidoService;
+import com.softexpert.divisaoconta.api.service.PicpayService;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -16,6 +18,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DivisaoContaTest {
 
     private CalculadoraPedidoService service = new CalculadoraPedidoService();
+
+    private PicpayService servicePagamento = new PicpayService();
 
     @Test
     void calcularTotalPagoPorItem() {
@@ -73,5 +77,34 @@ public class DivisaoContaTest {
 
         assertEquals(BigDecimal.valueOf(31.92), totalPagoAmigo1.setScale(2, RoundingMode.HALF_UP));
         assertEquals(BigDecimal.valueOf(6.08), totalPagoAmigo2.setScale(2, RoundingMode.HALF_UP));
+    }
+
+    @Test
+    void deveGerarLinkSimplificadoEAdicionarAoRetorno() {
+
+        Conta conta = new Conta();
+
+        Pedido pedido = new Pedido();
+        pedido.setItens(
+                List.of(
+                        new ItemPedido("Hamburguer", BigDecimal.valueOf(40.00), 1L),
+                        new ItemPedido("sobremesa", BigDecimal.valueOf(2.00), 1L)));
+
+
+        Pedido pedido1 = new Pedido();
+        pedido1.setItens(List.of(new ItemPedido("sanduiche", BigDecimal.valueOf(8.00), 1L)));
+
+        conta.setPedidos(List.of(pedido,pedido1));
+
+        final OutroValor desconto = new OutroValor("PORCENTAGEM", BigDecimal.valueOf(40.0), "DESCONTO");
+        final OutroValor acrescimo = new OutroValor("REAL", BigDecimal.valueOf(8.0), "ACRESCIMO");
+
+        conta.setOutrosValores(List.of(desconto,acrescimo));
+
+        List<DivisaoContaDTO> divisaoContaDTOS = service.calcularDivisaoConta(conta);
+
+        List<DivisaoContaDTO> divisaoContaComInformaçõesPagamento = servicePagamento.adicionarLinkPagamento(
+                divisaoContaDTOS, "@robsonmoroni");
+
     }
 }
